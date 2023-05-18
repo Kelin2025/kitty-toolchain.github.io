42e15d1d7aa854838c19b90f389c2ec3c3ed6e1f
Anton Kosykh <kelin2025@yandex.ru>
2023-05-18 14:47:14 +0300

    Remove noise link

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   outDir: "docs/.vitepress/dist",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
-    nav: [{ text: "Home", link: "/" }],
+    nav: [],
 
     sidebar: [
       {
